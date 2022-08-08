f89e0bd449718f5f914e96e36185e2c34d62d5ac
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-08-08 10:50:02 +0900

    💡 add darkThmes

---
+++ src/pages/_app.tsx
@@ -0,0 +1,49 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { GetServerSidePropsContext } from "next";
+import { useState } from "react";
+import { getCookie, setCookie } from "cookies-next";
+import {
+	MantineProvider,
+	ColorScheme,
+	ColorSchemeProvider,
+} from "@mantine/core";
+
+function MyApp(props: AppProps & { colorScheme: ColorScheme }) {
+	const [colorScheme, setColorScheme] = useState<ColorScheme>(
+		props.colorScheme
+	);
+
+	const toggleColorScheme = (value?: ColorScheme) => {
+		const nextColorScheme =
+			value || (colorScheme === "dark" ? "light" : "dark");
+		setColorScheme(nextColorScheme);
+		setCookie("mantine-color-scheme", nextColorScheme, {
+			maxAge: 60 * 60 * 24 * 30,
+		});
+	};
+
+	const { Component, pageProps } = props;
+
+	return (
+		<ColorSchemeProvider
+			colorScheme={colorScheme}
+			toggleColorScheme={toggleColorScheme}
+		>
+			<MantineProvider
+				theme={{ colorScheme }}
+				withGlobalStyles
+				withNormalizeCSS
+			>
+				<Component {...pageProps} />
+			</MantineProvider>
+		</ColorSchemeProvider>
+	);
+}
+
+MyApp.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+	// get color scheme from cookie
+	colorScheme: getCookie("mantine-color-scheme", ctx) || "light",
+});
+
+export default MyApp;
