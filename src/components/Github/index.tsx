9d68a77b72f6cff7727f05571e0aaa5d6aedfd2b
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-08-17 22:19:16 +0900

    🌱 add github components

---
+++ src/components/Github/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@mantine/core";
+import { FC } from "react";
+
+export const Github: FC = () => {
+	return (
+		<div className="">
+			<Text className="text-4xl">Github</Text>
+		</div>
+	);
+};
