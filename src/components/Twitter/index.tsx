6062e54d4f52483ad3379ba4f6706caa9a6934a7
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-08-17 22:19:35 +0900

    🌱 add Twitter Components

---
+++ src/components/Twitter/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@mantine/core";
+import { FC } from "react";
+
+export const Twitter: FC = () => {
+	return (
+		<div>
+			<Text className="text-4xl">Twitter</Text>
+		</div>
+	);
+};
