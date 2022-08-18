9bde1fbe3a14ceaf636acfcd87baf82f177727a2
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-08-18 20:44:57 +0900

    🌱 add footer.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,12 @@
-export const Footer = () => {
+import { Box, Container, Text } from "@mantine/core";
+import { FC } from "react";
+
+export const Footer: FC = () => {
 	return (
-		<footer className="flex flex-1 justify-center py-4">
-			<small>&copy; {`${new Date().getFullYear()} foobar.`}</small>
-		</footer>
+		<div className="border-t-1 mt-20 flex h-16 items-center justify-center border-b-0 border-l-0 border-r-0 border-solid border-gray-100 pt-10 pb-10">
+			<Text weight={700} size={10} className="text-gray-500" color="#868E96">
+				<small>© ️2022 Shimabu IT University</small>
+			</Text>
+		</div>
 	);
 };
