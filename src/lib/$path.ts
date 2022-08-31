87f79e230bb6fc07991681e6ea4f18e8ec70e3f2
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-08-31 14:25:20 +0900

    🏷️  add oathpida type files.

---
+++ src/lib/$path.ts
@@ -0,0 +1,23 @@
+export const pagesPath = {
+  "about": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/about' as const, hash: url?.hash })
+  },
+  "blog": {
+    "BlogContents": {
+      $url: (url?: { hash?: string }) => ({ pathname: '/blog/BlogContents' as const, hash: url?.hash })
+    },
+    _id: (id: string | number) => ({
+      $url: (url?: { hash?: string }) => ({ pathname: '/blog/[id]' as const, query: { id }, hash: url?.hash })
+    }),
+    $url: (url?: { hash?: string }) => ({ pathname: '/blog' as const, hash: url?.hash })
+  },
+  "contact": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/contact' as const, hash: url?.hash })
+  },
+  "portfolio": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/portfolio' as const, hash: url?.hash })
+  },
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+}
+
+export type PagesPath = typeof pagesPath
---
+++ utils/$path.ts
@@ -0,0 +1,46 @@
+export const pagepath = {
+	about: {
+		$url: (url?: { hash?: string }) => ({
+			pathname: "/about" as const,
+			hash: url?.hash,
+		}),
+	},
+	blog: {
+		_id: (id: string | number) => ({
+			$url: (url?: { hash?: string }) => ({
+				pathname: "/blog/[id]" as const,
+				query: { id },
+				hash: url?.hash,
+			}),
+		}),
+		$url: (url?: { hash?: string }) => ({
+			pathname: "/blog" as const,
+			hash: url?.hash,
+		}),
+	},
+	contact: {
+		$url: (url?: { hash?: string }) => ({
+			pathname: "/contact" as const,
+			hash: url?.hash,
+		}),
+	},
+	portfolio: {
+		_id: (id: string | number) => ({
+			$url: (url?: { hash?: string }) => ({
+				pathname: "/portfolio/[id]" as const,
+				query: { id },
+				hash: url?.hash,
+			}),
+		}),
+		$url: (url?: { hash?: string }) => ({
+			pathname: "/portfolio" as const,
+			hash: url?.hash,
+		}),
+	},
+	$url: (url?: { hash?: string }) => ({
+		pathname: "/" as const,
+		hash: url?.hash,
+	}),
+};
+
+export type PagesPath = typeof pagepath;
