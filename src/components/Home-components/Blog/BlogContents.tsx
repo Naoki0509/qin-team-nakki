810d92025dff7afb099074fc4c36c9a9f955a46c
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-09-05 13:38:50 +0900

    🌱 add BlogCard.

---
+++ src/components/Home-components/Blog/BlogContents.tsx
@@ -0,0 +1,17 @@
+import { Card, Text } from "@mantine/core";
+import { FC } from "react";
+import { BlogItemProps } from "src/types";
+
+export const BlogContents: FC<BlogItemProps> = (props) => {
+	return (
+		<div>
+			<Card
+				key={props.id}
+				className="shadow-lg border border-gray-100 border-solid"
+			>
+				<Text className="font-bold">{props.title}</Text>
+				<Text>{props.subtitle}</Text>
+			</Card>
+		</div>
+	);
+};
