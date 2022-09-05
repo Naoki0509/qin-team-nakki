15994302ed7ed0324f3514e9efffbc75e39fe18d
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-09-05 13:45:46 +0900

    ♻️ BlogComponent化によるファクタ

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,35 +1,27 @@
-import { Container, Grid, Group, Text, Title } from "@mantine/core";
-import { GetStaticProps, NextPage } from "next";
-import Link from "next/link";
-import { ContentLayout } from "src/components/Home-components/ContentLayout";
-import { client } from "src/lib/client";
-import { BlogProps, MicroCMSBlogProps } from "src/types";
+import { Container } from "@mantine/core";
 
-const Blog: NextPage<MicroCMSBlogProps> = (props) => {
+import { GetStaticProps, NextPage } from "next";
+import { Blog } from "src/components/Home-components/Blog/blog";
+
+import { Layout } from "src/components/Layout";
+import { client } from "src/lib/client";
+import { BlogItemProps, BlogProps } from "src/types";
+
+export const Home: NextPage<BlogProps> = (props) => {
 	return (
-		<ContentLayout title="Blog">
-			<Grid className="flex flex-col items-center justify-center space-y-10">
-				{props.contents.map((content) => {
-					return (
-						<Link href={`/blog/${content.id}`}>
-							<Grid.Col className=" border-solid border-gray-200 shadow-lg min-w-full py-6">
-								<Title order={4}>{content.title}</Title>
-								<Text>{content.subtitle}</Text>
-							</Grid.Col>
-						</Link>
-					);
-				})}
-			</Grid>
-		</ContentLayout>
+		<Layout title="Blog">
+			<Container>
+				<Blog {...props} />
+			</Container>
+		</Layout>
 	);
 };
 
-export const getStaticProps: GetStaticProps<MicroCMSBlogProps> = async () => {
-	const data = await client.getList<BlogProps>({ endpoint: "blogs" });
-
+export const getStaticProps: GetStaticProps<BlogProps> = async () => {
+	const data = await client.getList<BlogItemProps>({ endpoint: "blogs" });
 	return {
 		props: data,
 	};
 };
 
-export default Blog;
+export default Home;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
 import { Box, Container } from "@mantine/core";
-import type { NextPage } from "next";
-import { Blog } from "src/components/Home-components/Blog";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import type { GetStaticProps, NextPage } from "next";
+import { Blog } from "src/components/Home-components/Blog/blog";
 import { Github } from "src/components/Home-components/Github";
 import { Portfolio } from "src/components/Home-components/portfolio";
 import { Twitter } from "src/components/Home-components/Twitter";
 import { Layout } from "src/components/Layout";
+import { client } from "src/lib/client";
 import { useMediaQuery } from "src/lib/mantine";
+import { BlogItemProps } from "src/types";
 
-const Home: NextPage = () => {
+type Props = {
+	blogdata: MicroCMSListResponse<BlogItemProps>;
+};
+
+const Home: NextPage<Props> = ({ blogdata }) => {
 	const response = useMediaQuery("sm");
 	return (
 		<Layout title="Home">
 			<Container size="md">
-				<Blog />
+				<Blog {...blogdata} />
 				<Portfolio />
 				<Box
 					className={response ? "flex justify-between space-x-10" : undefined}
@@ -26,3 +33,16 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+	const blogdata = await client.getList<BlogItemProps>({
+		endpoint: "blogs",
+		queries: { limit: 5 },
+	});
+
+	return {
+		props: {
+			blogdata,
+		},
+	};
+};
