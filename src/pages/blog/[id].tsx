ff80c86782c9c28bb0eb3deba1257ffc19b4cc6e
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-09-05 14:08:25 +0900

    💡 TypographyStylesProviderの追加

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,9 +1,11 @@
 import { Box, Container, Group, Text } from "@mantine/core";
+import dayjs from "dayjs";
 import { MicroCMSContentId, MicroCMSDate } from "microcms-js-sdk";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { Layout } from "src/components/Layout";
 import { client } from "src/lib/client";
 import { BlogPageProps } from "src/types";
+import { TypographyStylesProvider } from "@mantine/core";
 
 type Props = BlogPageProps & MicroCMSContentId & MicroCMSDate;
 
@@ -11,16 +13,19 @@ export const BlogID: NextPage<Props> = (props) => {
 	return (
 		<Layout title="Blog">
 			<Container size="md">
-				<Group className="flex items-center justify-center min-w-full">
-					<Text weight={700} className="text-2xl pt-[100px] pb-[50px]">
+				<Group className=" min-w-full flex-col">
+					<Text weight={700} className="text-2xl pt-[100px]">
 						{props.title}
 					</Text>
+					<time>{dayjs(props.createAt).format("YYYY.MM.DD")}</time>
 				</Group>
 
-				<Box
-					className="  border-t-2 border-b-0 border-r-0 border-l-0 border-solid pt-[50px] border-gray-100"
-					dangerouslySetInnerHTML={{ __html: props.content }}
-				/>
+				<TypographyStylesProvider>
+					<div
+						className="  border-t-2 border-b-0 border-r-0 border-l-0 border-solid pt-[50px] border-gray-100"
+						dangerouslySetInnerHTML={{ __html: props.content }}
+					/>
+				</TypographyStylesProvider>
 			</Container>
 		</Layout>
 	);
