cfd6c63ac7681f3f6a89a5105be0b6f394ef405d
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-09-08 19:10:35 +0900

    💡

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,11 +1,13 @@
-import { Box, Container, Group, Text } from "@mantine/core";
+import { Anchor, Box, Button, Container, Group, Text } from "@mantine/core";
 import dayjs from "dayjs";
 import { MicroCMSContentId, MicroCMSDate } from "microcms-js-sdk";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { Layout } from "src/components/Layout";
-import { client } from "src/lib/client";
+import { MicroClient } from "src/lib/client";
 import { BlogPageProps } from "src/types";
 import { TypographyStylesProvider } from "@mantine/core";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 type Props = BlogPageProps & MicroCMSContentId & MicroCMSDate;
 
@@ -13,26 +15,34 @@ export const BlogID: NextPage<Props> = (props) => {
 	return (
 		<Layout title="Blog">
 			<Container size="md">
-				<Group className=" min-w-full flex-col">
+				<Box className="min-w-full flex flex-col py-10 space-y-5">
 					<Text weight={700} className="text-2xl pt-[100px]">
 						{props.title}
 					</Text>
-					<time>{dayjs(props.createAt).format("YYYY.MM.DD")}</time>
-				</Group>
+					<Text className="text-[12px]">{`作成日　${dayjs(
+						props.createAt
+					).format("YYYY年MM月DD日")}`}</Text>
+				</Box>
 
 				<TypographyStylesProvider>
 					<div
-						className="  border-t-2 border-b-0 border-r-0 border-l-0 border-solid pt-[50px] border-gray-100"
+						className="border-t-2 border-b-0 border-r-0 border-l-0 border-solid pt-[50px] border-gray-100"
 						dangerouslySetInnerHTML={{ __html: props.content }}
 					/>
 				</TypographyStylesProvider>
+
+				<Link href="/blog" passHref>
+					<Anchor component="a" className="hover:text-blue-600 font-bold pt-8">
+						←back
+					</Anchor>
+				</Link>
 			</Container>
 		</Layout>
 	);
 };
 
 export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
-	const data = await client.getList({ endpoint: "blogs" });
+	const data = await MicroClient.getList({ endpoint: "blogs" });
 	const ids = data.contents.map((contents) => `/blog/${contents.id}`);
 	return {
 		paths: ids,
@@ -48,7 +58,7 @@ export const getStaticProps: GetStaticProps<Props, { id: string }> = async (
 			notFound: true,
 		};
 	}
-	const data = await client.getListDetail<BlogPageProps>({
+	const data = await MicroClient.getListDetail<BlogPageProps>({
 		endpoint: "blogs",
 		contentId: ctx.params.id,
 	});
